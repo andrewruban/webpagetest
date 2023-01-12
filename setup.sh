11a6911eb105c987017334bb6c3f2c3443222b43
Andrew <andrii.ruban@pm.bet>
2023-01-12 14:28:56 +0200

    feat - update setup script

--- setup.sh
+++ setup.sh
@@ -26,15 +26,15 @@ sudo docker-compose -v
 cd /opt/webpagetest
 sudo mv webpagetest.service /etc/systemd/system/webpagetest.service
 
+# Add www-data user permissions to a folder where you want to write results
+sudo mkdir -p /root/wpt_results
+sudo chown -R www-data /root/wpt_results
+
 # Start web-page-test systemd service
 sudo systemctl daemon-reload
 sudo systemctl start webpagetest.service
 sudo systemctl enable webpagetest.service
 sudo systemctl status webpagetest.service
-
-# Add www-data user permissions to a folder where you want to write results
-sudo mkdir -p /root/wpt_results
-sudo chown -R www-data /root/wpt_results
 
 # Add web-page-test log clearing by cron
 echo "5 2 * * * root find /root/wpt_results -type f -mtime +62 -exec rm -f {} \;" >> /etc/crontab
