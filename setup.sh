cad0376d590420ca41e6a749e10aeb9b98fb640c
Andrew <andrii.ruban@pm.bet>
2023-01-12 11:36:57 +0200

    feat - update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,28 @@
 #! /usr/bin/env bash
 set -euox pipefail
 
+# Install required software
+# docker-ce ( https://docs.docker.com/engine/install/ubuntu/ )
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker -v
+
+# docker-compose
+sudo apt-get install -y docker-compose
+sudo docker-compose -v
+
 # create web-page-test systemd service
 cd /opt/webpagetest
 sudo mv webpagetest.service /etc/systemd/system/webpagetest.service
