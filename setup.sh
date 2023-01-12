47d23f3494de407f3445b64a0d146a7f16a0c9f7
Andrew <andrii.ruban@pm.bet>
2023-01-12 11:56:58 +0200

    feat - update setup script

--- setup.sh
+++ setup.sh
@@ -38,5 +38,5 @@ sudo mkdir -p /root/wpt_results
 sudo chown -R www-data /root/wpt_results
 
 # Add web-page-test log clearing by cron
-echo "5 2 * * * find /root/wpt_results -type f -mtime +62 -exec rm -f {} \;" | tee -a /var/spool/cron/root
-echo "5 3 * * * find /root/wpt_results -type d -empty -mtime +62 -exec rmdir {} \;" >> /var/spool/cron/root
+echo "5 2 * * * root find /root/wpt_results -type f -mtime +62 -exec rm -f {} \;" >> /etc/crontab
+echo "5 3 * * * root find /root/wpt_results -type d -empty -mtime +62 -exec rmdir {} \;" >> /etc/crontab
