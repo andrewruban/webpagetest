f5bf81ab23a0e7f21a07282842e09f90efcb7bab
Andrew <andrii.ruban@pm.bet>
2023-01-12 11:59:17 +0200

    feat - update setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ set -euox pipefail
 
 # Install required software
 # docker-ce ( https://docs.docker.com/engine/install/ubuntu/ )
-sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install \
     ca-certificates \
